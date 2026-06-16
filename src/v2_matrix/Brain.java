cefc9126038cdbd5ad4d7073ac0486aecf05e49c
Anis <anis.baimukanov@gmail.com>
2026-06-16 14:57:21 +0200

    First working Brain version (implemented fully using LA, weights are normalized but there is not activation threshold yet)

--- src/v2_matrix/Brain.java
+++ src/v2_matrix/Brain.java
@@ -3,26 +3,67 @@ package v2_matrix;
 import math.Matrix;
 import math.Vector;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Brain {
-    private final Matrix association;
+    private Matrix weights;
 
-    private static float LEARNING_RATE = 0.3f;
+    // LEARNING_RATE must be between 0 (no learning) and 1 (immediate learning)
+    private float LEARNING_RATE = 0.2f;
 
-    public Brain (Matrix association) {
-        this.association = association;
+    public Brain (Matrix weights) {
+        this.weights = weights;
     }
 
-    public Brain (Matrix association, float LEARNING_RATE) {
-        this.association = association;
-        this.LEARNING_RATE = LEARNING_RATE;
+    public Brain (Matrix weights, float LEARNING_RATE) {
+        this.weights = weights;
+        if (LEARNING_RATE >= 0.0f && LEARNING_RATE <= 1.0f) {
+            this.LEARNING_RATE = LEARNING_RATE;
+        }
+        else {
+            throw new InvalidLearningRateException("Learning rate must be between 0 and 1");
+        }
     }
 
-    private Vector calculateOutput (Vector inputNeurons) {
-        return association.multiply(inputNeurons);
+    private Vector calculateOutputNeurons (Vector inputNeurons) {
+        return weights.multiply(inputNeurons);
     }
 
+    private Matrix calculateDeltaWeights (Vector inputNeurons, Vector outputNeurons) {
+        Matrix deltaWeights = (outputNeurons.outerProduct(inputNeurons)).multiply(LEARNING_RATE);
+        return deltaWeights;
+    }
 
+    private Matrix normalizeDeltaWeights (Matrix deltaWeights) {
+        Matrix availableCapacity = (this.weights.multiply(-1)).HadamardAddition(1);
+        Matrix normalizedDeltaWeights = deltaWeights.HadamardProduct(availableCapacity);
+
+        return normalizedDeltaWeights;
+    }
+
+    private void updateWeights (Matrix normalizedDeltaWeights) {
+        this.weights = this.weights.add(normalizedDeltaWeights);
+    }
+
+    private void simulateCycle(Vector inputNeurons) {
+        Vector outputNeurons = this.calculateOutputNeurons(inputNeurons);
+        Matrix deltaWeights = this.calculateDeltaWeights(inputNeurons, outputNeurons);
+        Matrix normalizedDeltaWeights = this.normalizeDeltaWeights(deltaWeights);
+        this.updateWeights(normalizedDeltaWeights);
+    }
+
+    public static void main(String[] args) {
+        float[] weights_array = {1.0f, 0.0f};
+        Matrix weights = new Matrix(weights_array, 1 , 2);
+        Brain brain = new Brain(weights);
+
+        float[] input_array = {1.0f, 1.0f};
+        Vector inputNeurons = new Vector(input_array);
+
+        brain.simulateCycle(inputNeurons);
+        brain.simulateCycle(inputNeurons);
+        brain.simulateCycle(inputNeurons);
+        brain.simulateCycle(inputNeurons);
+        brain.simulateCycle(inputNeurons);
+        brain.simulateCycle(inputNeurons);
+        brain.weights.printMatrix();
+    }
 }
---
+++ src/v2_matrix/InvalidLearningRateException.java
@@ -0,0 +1,7 @@
+package v2_matrix;
+
+public class InvalidLearningRateException extends RuntimeException {
+    public InvalidLearningRateException(String message) {
+        super(message);
+    }
+}
