52b6e78e7094d5a0aa2abe4987e159ae5b2232e1
Anis <anis.baimukanov@gmail.com>
2026-06-18 15:18:54 +0200

    Commented the code

--- src/math/Matrix.java
+++ src/math/Matrix.java
@@ -3,45 +3,61 @@ package math;
 import Exceptions.InvalidMatrixDimensionsException;
 
 public class Matrix {
+    // A matrix is stored as a 2D array with row-major ordering
     private final float[][] matrix;
+    // The number of rows and columns are stored for convenience
     private final int numRows;
     private final int numColumns;
 
+    // Constructor
     public Matrix (float[] entries, int numRows, int numColumns) {
+        // Check whether the number of rows and columns match the entries array
         if ((numRows * numColumns) != entries.length) {
             throw new InvalidMatrixDimensionsException("Matrix dimensions don't match the entries");
         }
 
+        // Set up the fields
         this.numRows = numRows;
         this.numColumns = numColumns;
         this.matrix = new float[numRows][numColumns];
 
+        // Fill the 2D array based on 1D entries array
         for (int r = 0; r < numRows; r++) {
             for (int c = 0; c < numColumns; c++) {
+                // Calculate the index of 1D array that corresponds to this row and column of 2D array
                 int arrayIndex = r * numColumns + c;
                 this.matrix[r][c] = entries[arrayIndex];
             }
         }
     }
 
+    // Getter for the 2D array of the matrix
+    // The inner 2D array is given as a copy to avoid shared mutability errors
     public float[][] getMatrix () {
         return this.copy().matrix;
     }
 
+    // Getter for the number of rows
     public int getNumRows () {return this.numRows; }
 
+    // Getter for the number of columns
     public int getNumColumns () {return this.numColumns; }
 
+    // Add another matrix to this matrix
+    // The resulting matrix is returned as a separate new matrix
     public Matrix add (Matrix m) {
+        // Check whether the dimensions match
         if (this.numRows != m.numRows || this.numColumns != m.numColumns) {
             throw new InvalidMatrixDimensionsException("Addition is undefined as matrix dimensions do not match");
         }
 
+        // Set up the fields for the resulting matrix
         int newNumRows = this.numRows;
         int newNumColumns = this.numColumns;
         int newNumEntries = newNumRows * newNumColumns;
         float[] newEntries = new float[newNumEntries];
 
+        // Perform the addition
         for (int r = 0; r < newNumRows; r++) {
             for (int c = 0; c < newNumColumns; c++) {
                 int newIndex = r * newNumColumns + c;
@@ -52,7 +68,10 @@ public class Matrix {
         return new Matrix (newEntries, newNumRows, newNumColumns);
     }
 
+    // Subtract another matrix from this matrix
+    // Substraction is implemented as addition with another matrix multiplied by -1
     public Matrix subtract(Matrix m) {
+        // Check whether the dimensions match
         if (this.numRows != m.numRows || this.numColumns != m.numColumns) {
             throw new InvalidMatrixDimensionsException("Subtraction is impossible as matrix dimensions do not match");
         }
@@ -62,6 +81,8 @@ public class Matrix {
         return this.add(mNegated);
     }
 
+    // Multiply this matrix by a constant
+    // The multiplication is performed on the copy of the inner 2D array to avoid shared mutability errors
     public Matrix multiply (float n) {
         Matrix m = this.copy();
         for (int r = 0; r < m.numRows; r++) {
@@ -73,20 +94,27 @@ public class Matrix {
         return m;
     }
 
+    // Multiply this matrix by a vector
+    // Since a vector is a wrapper around matrix, just perform matrix-matrix multiplication
     public Vector multiply (Vector v) {
         return new Vector(this.multiply(v.vectorAsMatrix()));
     }
 
+    // Multiply this matrix by another matrix
+    // The resulting matrix is returned as a separate new matrix
     public Matrix multiply (Matrix m) {
+        // Check whether the dimensions are correct for the multiplication
         if (this.numColumns != m.numRows) {
             throw new InvalidMatrixDimensionsException("Matrices dimensions are invalid for multiplication");
         }
 
+        // Set up the fields for the resulting matrix
         int newNumRows = this.numRows;
         int newNumColumns = m.numColumns;
         int newNumEntries = newNumRows * newNumColumns;
         float[] newEntries = new float[newNumEntries];
 
+        // Perform the matrix-matrix multiplication
         for (int rLeft = 0; rLeft < this.numRows; rLeft++) {
             for (int cRight = 0; cRight < m.numColumns; cRight++) {
                 int newIndex = rLeft * newNumColumns + cRight;
@@ -103,12 +131,16 @@ public class Matrix {
         return new Matrix(newEntries, newNumRows, newNumColumns);
     }
 
+    // Transpose this matrix
+    // The resulting matrix is returned as a separate new matrix
     public Matrix transpose () {
+        // Set up the fields for the resulting matrix
         int newNumRows = this.numColumns;
         int newNumColumns = this.numRows;
         int newNumEntries = newNumRows * newNumColumns;
         float[] newEntries = new float[newNumEntries];
 
+        // Perform transposition
         for (int r = 0; r < this.numRows; r++) {
             for (int c = 0; c < this.numColumns; c++) {
                 int newArrayIndex = c * numRows + r;
@@ -119,16 +151,21 @@ public class Matrix {
         return new Matrix(newEntries, newNumRows, newNumColumns);
     }
 
+    // Perform Hadamard (element-wise) product of this matrix with another matrix
+    // The resulting matrix is returned as a separate new matrix
     public Matrix HadamardProduct (Matrix m) {
+        // Check whether the dimensions match
         if (this.numRows != m.numRows || this.numColumns != m.numColumns) {
             throw new InvalidMatrixDimensionsException("Addition is undefined as matrix dimensions do not match");
         }
 
+        // Set up the fields for the resulting matrix
         int newNumRows = this.numRows;
         int newNumColumns = this.numColumns;
         int newNumEntries = newNumRows * newNumColumns;
         float[] newEntries = new float[newNumEntries];
 
+        // Perform the Hadamard product
         for (int r = 0; r < newNumRows; r++) {
             for (int c = 0; c < newNumColumns; c++) {
                 int newIndex = r * newNumColumns + c;
@@ -139,12 +176,16 @@ public class Matrix {
         return new Matrix (newEntries, newNumRows, newNumColumns);
     }
 
+    // Perform the scalar shift (element-wise addition) of this matrix with a constant
+    // The resulting matrix is returned as a separate new matrix
     public Matrix scalarShift(float n) {
+        // Set up the fields for the resulting matrix
         int newNumRows = this.numRows;
         int newNumColumns = this.numColumns;
         int newNumEntries = newNumRows * newNumColumns;
         float[] newEntries = new float[newNumEntries];
 
+        // Perform the scalar shift
         for (int r = 0; r < newNumRows; r++) {
             for (int c = 0; c < newNumColumns; c++) {
                 int newIndex = r * newNumColumns + c;
@@ -155,6 +196,7 @@ public class Matrix {
         return new Matrix (newEntries, newNumRows, newNumColumns);
     }
 
+    // Print this matrix
     public void printMatrix () {
         StringBuilder matrixAsText = new StringBuilder("CEILING\n");
 
@@ -173,6 +215,7 @@ public class Matrix {
         System.out.println(matrixAsText);
     }
 
+    // Make a copy of this matrix
     public Matrix copy () {
         int newNumRows = this.numRows;
         int newNumColumns = this.numColumns;
--- src/v2_matrix/Benchmark.java
+++ src/v2_matrix/Benchmark.java
@@ -5,36 +5,49 @@ import math.Vector;
 
 public class Benchmark {
     public static void main(String[] args) {
+        // Benchmark to measure the time required for running method "simulateCycle"
+
+        // Choose the number of cycles
         int CYCLES = 100;
+        // Choose the dimensions of the square matrix
         int size = 3000;
 
+        // Fill the matrix with random numbers
         float[] weights_array = new float[size * size];
         for (int i = 0; i < weights_array.length; i++) {
             weights_array[i] = (float) Math.random();
         }
 
+        // Create the matrix and the brain
         Matrix weights_matrix = new Matrix(weights_array, size, size);
         Brain brain = new Brain(weights_matrix);
 
+        // Fill input vector with random values
         float[] input_array = new float[size];
         for (int i = 0; i < size; i++) {
             input_array[i] = (float) Math.random();
         }
 
+        // Create input vector
         Vector input_vector = new Vector(input_array);
 
-        // Warm-up
+        // Warm-up to invoke JIT compiler, so the true speed can be measured afterwards
         for (int i = 0; i < 50; i++) {
             brain.simulateCycle(input_vector);
         }
 
+        // Start the timer
         long start = System.nanoTime();
+        // Run simulations
         for (int i = 0; i < CYCLES; i++) {
             brain.simulateCycle(input_vector);
         }
+        // Stop the timer
         long end = System.nanoTime();
 
+        // Calculate the time taken to run simulations
         long time = (end - start) / 1_000_000;
+        // Print the time taken to run simulations
         System.out.println("RUNNING TIME (ms): " + time);
     }
 }
